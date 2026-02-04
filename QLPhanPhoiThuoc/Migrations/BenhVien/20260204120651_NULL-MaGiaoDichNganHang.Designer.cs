0cff9de7b729efbb5b993b03663b2175b3f58bc6
binh-ff9 <duybinh7700@gmail.com>
2026-02-04 20:54:29 +0700

    ADD: ChiTietHoaDon

--- QLPhanPhoiThuoc/Migrations/BenhVien/20260204120651_NULL-MaGiaoDichNganHang.Designer.cs
+++ QLPhanPhoiThuoc/Migrations/BenhVien/20260204120651_NULL-MaGiaoDichNganHang.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QLPhanPhoiThuoc.Models.EF;
 
@@ -11,9 +12,11 @@ using QLPhanPhoiThuoc.Models.EF;
 namespace QLPhanPhoiThuoc.Migrations.BenhVien
 {
     [DbContext(typeof(BenhVienDbContext))]
-    partial class BenhVienDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260204120651_NULL-MaGiaoDichNganHang")]
+    partial class NULLMaGiaoDichNganHang
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ QLPhanPhoiThuoc/Migrations/BenhVien/20260204120651_NULL-MaGiaoDichNganHang.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QLPhanPhoiThuoc.Migrations.BenhVien
+{
+    /// <inheritdoc />
+    public partial class NULLMaGiaoDichNganHang : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MaGiaoDichNganHang",
+                table: "VietQR_GiaoDich",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTao",
+                table: "TaiKhoan",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MaGiaoDichNganHang",
+                table: "VietQR_GiaoDich",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "NgayTao",
+                table: "TaiKhoan",
+                type: "datetime2",
+                nullable: true,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+        }
+    }
+}
